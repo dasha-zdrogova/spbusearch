dec8f3d21ff1e3fa279459cfb993f2c79bdced54
olya-antre <antropoffa@gmail.com>
2023-12-06 02:54:23 +0300

    fixed code for database

--- src/lib/data_base.py
+++ src/lib/data_base.py
@@ -9,6 +9,7 @@ try:
     #подключаемся к серверу
     with pymysql.connect(
         host = "127.0.0.1",
+        port = 9306,
         user='root',
         password='12345678',
     ) as connection:
@@ -27,6 +28,7 @@ try:
     #подключаемся к созданной базе данных
     with pymysql.connect(
         host = "127.0.0.1",
+        port = 9306,
         user='root',
         password='12345678',
         database="spbusearch"
@@ -35,34 +37,38 @@ try:
 
         #создаем таблицу, в которую будем сохранять инфу по файлам для индексации
         with connection.cursor() as cursor:
-            create_table_query = "CREATE TABLE files (id int AUTO_INCREMENT, file_name varchar(100), URL varchar(100), content text, PRIMARY KEY(id));"
+            #cursor.execute("DROP TABLE files")
+
+            create_table_query = "CREATE TABLE IF NOT EXISTS files (id int AUTO_INCREMENT, file_name varchar(400), URL varchar(400), content longtext, PRIMARY KEY(id));"
             cursor.execute(create_table_query)
             connection.commit()
 
 
         #чтение и сохранение данных из файлов формата docx (которые сохранили в папку после работы парсера)
-        for file in glob('**/*.docx', recursive=True):
-            text = read_docx(file)
-            url = file.split(sep='\\')[1]
-            file_name = file.split(sep='\\')[-1]
-            inserts = (file_name, url, text)
-            cursor.execute(
-                'INSERT INTO files (file_name, URL, content) VALUES (%s, %s, %s)', file_name, url, text
-            )
-            connection.commit()
+            for file in glob('**/*.docx', recursive=True):
+                text = read_docx(file)
+                url = file.split(sep='\\')[1]
+                file_name = file.split(sep='\\')[-1]
+                inserts = (file_name, url, text)
+                cursor.execute("INSERT INTO files (file_name, URL, content) VALUES (%s, %s, %s)", (file_name, url, text))
+                connection.commit()
 
 
         #чтение и сохранение данных из файлов формата pdf (которые сохранили в папку после работы парсера)
-        for file in glob('**/*.pdf', recursive=True):
-            text = ocr_pdf(file)
-            url = file.split(sep='\\')[1]
-            file_name = file.split(sep='\\')[-1]
-            cursor.execute(
-                'INSERT INTO files (file_name, URL, content) VALUES (%s, %s, %s)', file_name, url, text
-            )
-            connection.commit()
-
-
+            for file in glob('**/*.pdf', recursive=True):
+                text = ocr_pdf(file)
+                url = file.split(sep='\\')[1]
+                file_name = file.split(sep='\\')[-1]
+                cursor.execute(
+                    'INSERT INTO files (file_name, URL, content) VALUES (%s, %s, %s)', (file_name, url, text)
+                )
+                connection.commit()
+            
+            print("Добавлены следующие файлы:")
+            cursor.execute("SELECT file_name from files")
+            result = cursor.fetchall()
+            for row in result:
+                print(row)
 
 except Exception as e:
     print(e)
